e38d6297241286d8b46f4ff1f689d975c76851c9
redo033 <250150@newuu.uz>
2025-10-29 12:40:25 +0500

    hello world

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// float avgNumInArray(int numbers[], int size ) {
+//     float sum = 0;
+//     for (int i = 0; i < size; i++) {
+//         sum += (float)numbers[i];
+//     }
+//     return sum/size;
+// }
+// int main() {
+//     int n;
+//     cin >> n;
+//     int the_numbers[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> the_numbers[i];
+//     }
+//     cout<<"Avarage is"<<avgNumInArray(the_numbers,n);
+//     return 0;
+
+}
+//problem6
+bool isSmallAlphabet(char c) {
+    return('a' <= c && c <= 'z')?1:0;
+}
+bool isBigAlphabet(char c) {
+    return('A' <= c && c <= 'Z')?1:0;
+}
+bool isNumber(char c) {
+    return('0' <= c && c <= '9')?1:0;
+}
+    int main () {
+    int n; small=0, spec=0, digit=0;
+    cin>>n;
+}
